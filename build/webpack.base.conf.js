a8686907df688e4c9fe02c06cef3d078c006b76b
linhe <linhe@dtstack.com>
2019-06-25 17:32:19 +0800

    修改配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -119,7 +119,14 @@ let baseWebpackConfig = {
         to: path.resolve(config.build.assetsRoot, './static'),
         ignore: ['.*']
       }
-    ])
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../node_modules/vant-weapp/dist'),
+        to: path.resolve(config.build.assetsRoot, '../vant-weapp'),
+        ignore: ['.*']
+      }
+    ]),
   ]
 }
 
